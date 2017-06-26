cdccdcf2133f5c1986fc32d01665662535762566
trukhilio <trukhantech@gmail.com>
2017-06-26 23:53:32 +0300

    add connect

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class App extends Component {
+class App extends Component {
     render(){
         return(
-            <div> Hello
+            <div> Hello , {this.props.user}!
             </div>
         )
     }
 }
+
+function mapStateToProps (state) {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps)(App)
